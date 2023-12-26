3e4914a56562692476f414737314ebf3c05e0e5b
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2023-12-26 07:40:38 +0100

    change tabwidth prettier

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,19 +1,24 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-astro", "prettier-plugin-svelte","prettier-plugin-tailwindcss"],
-  overrides: [
-    {
-      files: "*.astro",
-      options: {
-        parser: "astro",
-      },
-    },
-    {
-      files: "*.svelte",
-      options: {
-        parser: "svelte",
-      },
-    },
-  ],
+    tabWidth: 4,
+    plugins: [
+        "prettier-plugin-astro",
+        "prettier-plugin-svelte",
+        "prettier-plugin-tailwindcss",
+    ],
+    overrides: [
+        {
+            files: "*.astro",
+            options: {
+                parser: "astro",
+            },
+        },
+        {
+            files: "*.svelte",
+            options: {
+                parser: "svelte",
+            },
+        },
+    ],
 };
