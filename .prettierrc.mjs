9f635f157327aad651d2f669c9ce59163a80f7ce
Sandro Das Neves <17152022+sandro-git@users.noreply.github.com>
2023-12-26 07:25:28 +0100

    add prettier and plugins

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,13 +1,19 @@
 // .prettierrc.mjs
 /** @type {import("prettier").Config} */
 export default {
-    plugins: ['prettier-plugin-astro'],
-    overrides: [
-      {
-        files: '*.astro',
-        options: {
-          parser: 'astro',
-        },
+  plugins: ["prettier-plugin-astro", "prettier-plugin-svelte","prettier-plugin-tailwindcss"],
+  overrides: [
+    {
+      files: "*.astro",
+      options: {
+        parser: "astro",
       },
-    ],
-  };
+    },
+    {
+      files: "*.svelte",
+      options: {
+        parser: "svelte",
+      },
+    },
+  ],
+};
---
+++ src/components/Header.svelte
@@ -0,0 +1,16 @@
+<header class=" flex items-center justify-between bg-gray-800 py-4 text-white">
+  <h1 class="text-lg font-bold">Mon titre</h1>
+  <nav>
+    <ul class="flex">
+      <li class="ml-4">
+        <a href="/" class="hover:text-gray-300">Accueil</a>
+      </li>
+      <li class="ml-4">
+        <a href="/" class="hover:text-gray-300">À propos</a>
+      </li>
+      <li class="ml-4">
+        <a href="/" class="hover:text-gray-300">Contact</a>
+      </li>
+    </ul>
+  </nav>
+</header>
